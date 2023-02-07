37033895627c59daf70abcd879d2646e96946390
Daniel Wagner <dtw@dropbox.com>
2023-02-07 11:21:17 +0000

    Use asyncio.run()

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import sys
 import time
 
 from collections import defaultdict
+from datetime import datetime
+from typing import Dict, List
 
 # Configure logging before initializing meross iot so we get timestamps
 import logging
@@ -16,9 +18,14 @@ logging.basicConfig(
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
 
+from meross_iot.controller.device import BaseDevice
 from meross_iot.controller.mixins.electricity import ElectricityMixin
 from meross_iot.http_api import MerossHttpClient
 from meross_iot.manager import MerossManager
+from meross_iot.model.enums import OnlineStatus
+from meross_iot.model.plugin.power import PowerInfo
+from meross_iot.model.push.generic import GenericPushNotification
+from meross_iot.model.push.online import OnlinePushNotification
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -34,136 +41,157 @@ SEMP2REST_ORIGIN = os.environ.get('SEMP2REST_ORIGIN')
 
 POLL_TIMEOUT_S = 1
 
-samples = defaultdict(list)
-sample_ts = defaultdict(list)
+class MerossSMA(object):
+    samples_by_dev_id: Dict[str, List[float]] = defaultdict(list)
+    sample_ts_by_dev_id: Dict[str, List[float]] = defaultdict(list)
 
-def device_id_from_uuid(uuid, channel):
-    serial = hashlib.md5(uuid.encode('utf-8')).hexdigest()[:12]
-    return f"F-11223344-{serial}-{channel:0>2d}"
+    connection_status_by_dev_id: Dict[str, OnlineStatus] = defaultdict(lambda: OnlineStatus.ONLINE)
+    disconnection_detected = False
 
-
-def set_last_power(device_id):
-    payload = {
-        "power": {
-            "Watts": sum(samples[device_id])/len(samples[device_id]),
-            "MinPower": min(samples[device_id]),
-            "MaxPower": max(samples[device_id]),
-        },
-    }
-    r = requests.put(f"{SEMP2REST_ORIGIN}/api/devices/{device_id}/lastPower", data=json.dumps(payload), headers={"Content-Type":"application/json"})
-    r.raise_for_status()
+    def device_id_from_uuid(self, uuid: str, channel: int) -> str:
+        serial = hashlib.md5(uuid.encode('utf-8')).hexdigest()[:12]
+        return f"F-11223344-{serial}-{channel:0>2d}"
 
 
-def register_device(dev):
-    payload = {
-        "device": {
-           "deviceId": device_id_from_uuid(dev.uuid, 0),
-           "name": dev.name,
-           "type": "Other", # AirConditioning, Charger, DishWasher, Dryer, ElectricVehicle, EVCharger, Freezer, Fridge, Heater, HeatPump, Motor, Pump, WashingMachine, Other
-           "measurementMethod": "Measurement",  # Measurement, Estimation, None
-           "interruptionsAllowed": False,
-           "maxPower": 3680,  # 230V 16A
-           "emSignalsAccepted": False,
-           "status": "On",  # On, Off, Offline
-           "vendor": "Meross",
-           "serialNr": dev.uuid,
-           "absoluteTimestamps": False,  # we don't accept scheduling events either way
-           "optionalEnergy": False,
-        },
-    }
-    r = requests.post(f"{SEMP2REST_ORIGIN}/api/devices", data=json.dumps(payload), headers={"Content-Type":"application/json"})
-    if r.status_code != 200:
-        logging.warning(f"Status {r.status_code} when creating device: {r.text}")
+    def set_last_power(self, device_id: str):
+        payload = {
+            "power": {
+                "Watts": sum(self.samples_by_dev_id[device_id])/len(self.samples_by_dev_id[device_id]),
+                "MinPower": min(self.samples_by_dev_id[device_id]),
+                "MaxPower": max(self.samples_by_dev_id[device_id]),
+            },
+        }
+        r = requests.put(f"{SEMP2REST_ORIGIN}/api/devices/{device_id}/lastPower", data=json.dumps(payload), headers={"Content-Type":"application/json"})
+        r.raise_for_status()
 
-async def fetch_and_update_device(dev):
-    device_id = device_id_from_uuid(dev.uuid, 0)
 
-    while len(sample_ts[device_id]) > 0 and sample_ts[device_id][0] < time.time() - 60:
-        samples[device_id].pop(0)
-        sample_ts[device_id].pop(0)
-    
-    try:
-        result = await dev.async_get_instant_metrics(timeout=POLL_TIMEOUT_S)
-    except Exception:
-        return None
+    def register_device(self, dev: BaseDevice):
+        payload = {
+            "device": {
+               "deviceId": self.device_id_from_uuid(dev.uuid, 0),
+               "name": dev.name,
+               "type": "Other", # AirConditioning, Charger, DishWasher, Dryer, ElectricVehicle, EVCharger, Freezer, Fridge, Heater, HeatPump, Motor, Pump, WashingMachine, Other
+               "measurementMethod": "Measurement",  # Measurement, Estimation, None
+               "interruptionsAllowed": False,
+               "maxPower": 3680,  # 230V 16A
+               "emSignalsAccepted": False,
+               "status": "On",  # On, Off, Offline
+               "vendor": "Meross",
+               "serialNr": dev.uuid,
+               "absoluteTimestamps": False,  # we don't accept scheduling events either way
+               "optionalEnergy": False,
+            },
+        }
+        r = requests.post(f"{SEMP2REST_ORIGIN}/api/devices", data=json.dumps(payload), headers={"Content-Type":"application/json"})
+        if r.status_code != 200:
+            logging.warning(f"Status {r.status_code} when creating device: {r.text}")
 
-    if result:
-        samples[device_id].append(result.power)
-        sample_ts[device_id].append(result.sample_timestamp.timestamp())
+    async def handle_push_notification(self, notification: GenericPushNotification, devices: List[BaseDevice], manager: MerossManager):
+        if isinstance(notification, OnlinePushNotification):
+            dev_id = self.device_id_from_uuid(notification.originating_device_uuid, 0)
+            self.connection_status_by_dev_id[dev_id] = notification.status
+            logging.info(f"Push notification: Device {dev_id} online status changed to {OnlineStatus(notification.status).name}")
 
-        # try updating lastPower, assuming device exists. Create the device if we get a 404 response
-        try:
-            set_last_power(device_id)
-        except requests.exceptions.HTTPError as e:
-            if e.response.status_code == 404:
-                register_device(dev)
+            for status in self.connection_status_by_dev_id.values():
+                if status != -1:
+                    break
             else:
+                logging.warning("All devices are in unknown status; disconnection event detected.")
+                self.disconnection_detected = True
+
+
+    async def fetch_and_update_device(self, dev) -> PowerInfo:
+        device_id = self.device_id_from_uuid(dev.uuid, 0)
+
+        while len(self.sample_ts_by_dev_id[device_id]) > 0 and self.sample_ts_by_dev_id[device_id][0] < time.time() - 60:
+            self.samples_by_dev_id[device_id].pop(0)
+            self.sample_ts_by_dev_id[device_id].pop(0)
+
+        try:
+            result = await dev.async_get_instant_metrics(timeout=POLL_TIMEOUT_S)
+        except Exception:
+            return None
+
+        if result:
+            self.samples_by_dev_id[device_id].append(result.power)
+            self.sample_ts_by_dev_id[device_id].append(result.sample_timestamp.timestamp())
+
+            # try updating lastPower, assuming device exists. Create the device if we get a 404 response
+            try:
+                self.set_last_power(device_id)
+            except requests.exceptions.HTTPError as e:
+                if e.response.status_code == 404:
+                    self.register_device(dev)
+                else:
+                    logging.warning(e)
+            except Exception as e:
+                # just print and struggle on
                 logging.warning(e)
-        except Exception as e:
-            # just print and struggle on
-            logging.warning(e)
 
-    return result
+        return result
 
-async def connect_and_forward():
-    """
-    Connects MerossIot and forwards readings via SEMP2REST.
-    Exits after no new measurement has been received for RECONNECT_TIME_S seconds
-    """
-    last_measurement = time.time()
-    last_status_print = 0
-    last_device_update = 0
-    devs = []
+    async def connect_and_forward(self) -> None:
+        """
+        Connects MerossIot and forwards readings via SEMP2REST.
+        Exits after no new measurement has been received for RECONNECT_TIME_S seconds
+        """
+        last_measurement = time.time()
+        last_status_print = 0.0
+        last_device_update = 0.0
+        devs = []
 
-    http_api_client = await MerossHttpClient.async_from_user_password(email=EMAIL, password=PASSWORD)
-    manager = MerossManager(http_client=http_api_client)
-    try:
-        await manager.async_init()
+        http_api_client = await MerossHttpClient.async_from_user_password(email=EMAIL, password=PASSWORD)
+        manager = MerossManager(http_client=http_api_client)
+        try:
+            manager.register_push_notification_handler_coroutine(self.handle_push_notification)
 
-        while time.time() - last_measurement < RECONNECT_TIME_S:
-            start = time.time()
+            connection_status_by_dev_id = defaultdict(lambda: OnlineStatus.ONLINE)  # type: Dict[str, OnlineStatus]
+            disconnection_detected = False
+            logging.info("Entering main loop...")
 
-            # periodically retrieve devices registered on this account
-            if time.time() - last_device_update > DISCOVER_FREQ_S:
-                await manager.async_device_discovery()
-                devs = manager.find_devices(device_class=ElectricityMixin)
-                last_device_update = time.time()
+            while (time.time() - last_measurement < RECONNECT_TIME_S) and (not self.disconnection_detected):
+                start = time.time()
 
-            updates = []
-            for dev in devs:
-                # TODO: might want to fetch these in parallel...
-                result = await fetch_and_update_device(dev)
-                if result:
-                    last_measurement = time.time()
+                # periodically retrieve devices registered on this account
+                if time.time() - last_device_update > DISCOVER_FREQ_S:
+                    await manager.async_device_discovery()
+                    devs = manager.find_devices(device_class=ElectricityMixin)
+                    last_device_update = time.time()
 
-            # periodically print stats
-            if STATUS_PRINT_FREQ_S > 0 and time.time() - last_status_print > STATUS_PRINT_FREQ_S:
-                logging.info("Device status")
                 for dev in devs:
-                    device_id = device_id_from_uuid(dev.uuid, 0)
-                    avg_watt = sum(samples[device_id])/len(samples[device_id])
-                    logging.info(f"  {dev.name} ({device_id}): {avg_watt:7.2f} W")
-                last_status_print = time.time()
-            
-            await asyncio.sleep(POLL_FREQUENCY_S - (time.time() - start))
+                    # TODO: might want to fetch these in parallel...
+                    result = await self.fetch_and_update_device(dev)
+                    if result:
+                        last_measurement = time.time()
 
-    finally:
-        # Close the manager and logout from http_api
-        manager.close()
-        await http_api_client.async_logout()
+                # periodically print stats
+                if STATUS_PRINT_FREQ_S > 0 and time.time() - last_status_print > STATUS_PRINT_FREQ_S:
+                    logging.info("Device status")
+                    for dev in devs:
+                        device_id = self.device_id_from_uuid(dev.uuid, 0)
+                        avg_watt = sum(self.samples_by_dev_id[device_id])/len(self.samples_by_dev_id[device_id])
+                        logging.info(f"  {dev.name} ({device_id}): {avg_watt:7.2f} W")
+                    last_status_print = time.time()
+
+                await asyncio.sleep(POLL_FREQUENCY_S - (time.time() - start))
+
+        finally:
+            # Close the manager and logout from http_api
+            logging.info("Exiting main loop...")
+            manager.close()
+            await http_api_client.async_logout()
 
 
 async def main():
+    forwarder = MerossSMA()
     while True:
         try:
-            await connect_and_forward()
+            await forwarder.connect_and_forward()
         except Exception as e:
             # just print and struggle on
             logging.warning(e)
 
 if __name__ == '__main__':
-    # Windows and python 3.8 requires to set up a specific event_loop_policy.
-    #  On Linux and MacOSX this is not necessary.
     if not EMAIL or not PASSWORD:
         print("Environment variables MEROSS_EMAIL or MEROSS_PASSWORD not set")
         sys.exit(1)
@@ -171,8 +199,4 @@ if __name__ == '__main__':
         print("Environment variable SEMP2REST_ORIGIN not set")
         sys.exit(1)
 
-    if os.name == 'nt':
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    loop.close()
+    asyncio.run(main())
